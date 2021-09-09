74a305ee0844c6b1b0a2f0c412fa21b684e39746
shihandimantha <76395781+shihandimantha@users.noreply.github.com>
2021-09-09 10:33:55 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,12 @@
+# Certificate-generator
+A Flask application for Bulk certificate generation
+
+# deploy your application
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/gokulapap/certificate-generator)
+
+# demo video
+
+https://user-images.githubusercontent.com/57899332/130783112-e7519445-0f5e-436a-ad5a-f1fa77d14613.mp4
+
+
---
+++ app.py
@@ -0,0 +1,53 @@
+from flask import Flask, render_template, send_file, url_for
+from PIL import Image, ImageDraw, ImageFont
+import pandas as pd
+import os
+
+font = ImageFont.truetype("arial.ttf", 50)
+
+W = 1000
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+	return '''
+	<center>
+	<u><h1>Certificate Generator</h1></u>
+	<br><br>
+
+	<h3>Add this to the URL to generate your certificate <br><br> /certificate/{name}</h3>
+
+	</center>
+	'''
+
+# add your app url in the above content 
+
+@app.route("/certificate/<name>")
+def certificate(name):
+	img = Image.open('/app/certificate.jpg')
+	draw = ImageDraw.Draw(img)
+	w, h = draw.textsize(name)
+	draw.text(xy=((W-w)/2 - w/2,360),text='{}'.format(name),fill=(0,0,0),font=font)
+	# must add the absolute path of the folders
+	img.save('/app/static/images/{}.jpg'.format(name))
+	f = open("/app/templates/{}.html".format(name), "w")
+	f.write('''
+	<html>
+	<head>
+	</head>
+	<body>
+	<br><br><br>
+	<center>
+	<img style="max-width: 99%; max-height: 99%;" src="/static/images/{}.jpg">
+	</center>
+	</body>
+	</html>
+	'''.format(name))
+	f.close()
+	return render_template("{}.html".format(name))
+
+if __name__ == "__main__":
+	app.run(port=5000)
+
+
